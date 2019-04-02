b779830c26ddcb1815e214dd8c90d57cd07131d3
guofeng1140559057 <guofeng1140559057@163.com>
2019-04-02 10:00:33 +0800

    Initial commit

---
+++ Chapter9-2-2/compute-service/compute-api-server/src/main/java/com/didispace/service/impl/ComputeServiceImpl.java
@@ -0,0 +1,16 @@
+package com.didispace.service.impl;
+
+import com.didispace.service.ComputeService;
+
+/**
+ * Created by zhaiyc on 2016/7/14.
+ */
+public class ComputeServiceImpl implements ComputeService {
+
+    @Override
+    public Integer add(int a, int b) {
+        System.out.println("进入计算");
+        return a + b;
+    }
+
+}
